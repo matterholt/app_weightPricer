03982dd1f63e52ef2de87a87b1828e9403408eaf
MAtterholt <matterholt.dev@gmail.com>
2026-07-06 08:34:53 -0400

    Update AnimalDialog.svelte

--- myappsvelte/src/lib/components/feature/animal_dialog/AnimalDialog.svelte
+++ myappsvelte/src/lib/components/feature/animal_dialog/AnimalDialog.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
 	import type { LiveStockEntry } from '../../../daTypes/livestock';
 	import { marketPricesState } from '../../../states/useMarketAveragePrice.svelte';
 	import { pricePerWeight } from '../../../utils/livestockActions';
@@ -16,19 +17,8 @@
 	});
 
 	let editMode: boolean = $state(false);
+	let editEntry = $state('');
 </script>
-
-{#snippet editModeForm()}
-	<div>
-		<EditInput inputType="text" label="Weight" value={animalDetails?.weight} />
-		<EditInput inputType="text" label="Visual ID" value={animalDetails?.visual_id} />
-		<EditInput inputType="text" label="Gender" value={animalDetails?.gender} />
-		<EditInput inputType="text" label="Cash" value={animalDetails?.profitedCash} />
-		<div class="modal-action">
-			<button class="btn" onclick={() => (editMode = !editMode)}>save & close </button>
-		</div>
-	</div>
-{/snippet}
 
 <dialog id="my-dialog" class="modal">
 	<div class="modal-box">
@@ -40,25 +30,41 @@
 			</p>
 		</div>
 		<div class="divider"></div>
-		{#if editMode}
-			{@render editModeForm()}
-		{:else}
-			<div class="grid grid-cols-2 grid-rows-4">
-				<p>Weight: {animalDetails?.weight}</p>
-				<p>Visual ID: {animalDetails?.visual_id}</p>
-				<p>Gender: {animalDetails?.gender}</p>
-				<p>Sell Price: ${animalDetails?.profitedCash}</p>
-				<div class="col-start-2 row-start-1 row-span-4 flex flex-col gap-5">
-					<h4 class="font-bold">Price Analysis</h4>
-					<p class="flex justify-between">Low: <span>${priceAnalysis?.lowCost}</span></p>
-					<p class="flex justify-between">Average: <span>${priceAnalysis?.averageCost}</span></p>
-					<p class="flex justify-between">High: <span>${priceAnalysis?.highCost}</span></p>
-				</div>
+
+		<div class="grid grid-cols-2 grid-rows-4">
+			<p>Visual ID: {animalDetails?.visual_id}</p>
+			<p>Gender: {animalDetails?.gender}</p>
+
+			{#if editEntry === 'animalWeight'}
+				<EditInput inputType="number" label="Weight" value={animalDetails?.weight} {editEntry} />
+			{:else}
+				<button class="flex gap-5 p-2" onclick={() => (editEntry = 'animalWeight')}
+					>Weight: <span>
+						{animalDetails?.weight}
+					</span></button
+				>
+			{/if}
+
+			{#if editEntry === 'profitedCash'}
+				<EditInput inputType="number" label="Sell Price" value={animalDetails?.profitedCash} />
+			{:else}
+				<button class="flex gap-5 p-2" onclick={() => (editEntry = 'profitedCash')}
+					>Sell Price: <span>
+						{animalDetails?.profitedCash}
+					</span></button
+				>
+			{/if}
+
+			<div class="col-start-2 row-start-1 row-span-4 flex flex-col gap-5">
+				<h4 class="font-bold">Price Analysis</h4>
+				<p class="flex justify-between">Low: <span>${priceAnalysis?.lowCost}</span></p>
+				<p class="flex justify-between">Average: <span>${priceAnalysis?.averageCost}</span></p>
+				<p class="flex justify-between">High: <span>${priceAnalysis?.highCost}</span></p>
 			</div>
-			<div class="modal-action">
-				<button class="btn" onclick={() => (editMode = !editMode)}>Edit</button>
-				<button class="btn" commandfor="my-dialog" command="close">Close</button>
-			</div>
-		{/if}
+		</div>
+		<div class="modal-action">
+			<button class="btn" onclick={() => (editMode = !editMode)}>Edit</button>
+			<button class="btn" commandfor="my-dialog" command="close">Close</button>
+		</div>
 	</div>
 </dialog>
