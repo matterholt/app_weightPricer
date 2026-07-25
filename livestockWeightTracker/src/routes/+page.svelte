8b7f4ded54c928cc62f59e7c9a89bf20c79898d1
MAtterholt <matterholt.dev@gmail.com>
2026-07-25 09:50:16 -0400

    sveltekit

---
+++ livestockWeightTracker/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+Tailwind Alert Component – daisyUITailwind Alert Component – daisyUI
+
+
+<div role="alert" class="alert alert-success">
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+  <span>Your purchase has been confirmed!</span>
+</div>
