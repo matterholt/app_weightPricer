b7c85c2f292e44e79e364910d25c23327622942a
MAtterholt <matterholt.dev@gmail.com>
2026-07-25 12:13:00 -0400

    carry over some comps

--- livestockWeightTracker/src/routes/+layout.svelte
+++ livestockWeightTracker/src/routes/+layout.svelte
@@ -2,10 +2,18 @@
     import "../style/app.css";
 	import favicon from '$lib/assets/favicon.svg';
 	let { children } = $props();
+
+	let activeTab = $state('available')
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+    <div role="tablist" class="tabs tabs-border">
+      <a href="livestock" role="tab" class="tab">Tab 1</a>
+      <a href="market_report" role="tab" class="tab tab-active">Tab 2</a>
+      <a href="available"role="tab" class="tab">Tab 3</a>
+      <a href="sold"role="tab" class="tab">Tab 3</a>
+    </div>
 {@render children()}
--- livestockWeightTracker/src/routes/+page.svelte
+++ livestockWeightTracker/src/routes/+page.svelte
@@ -1,11 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-Tailwind Alert Component – daisyUITailwind Alert Component – daisyUI
+<script module>
+	import AuctionMarketReport from '$lib/components/feature/market_report/AuctionMarketReport.svelte';
+	import LivestockTable from '$lib/components/feature/LivestockTable.svelte';
+	import NewLivestock from '$lib/components/feature/adding_livestock/NewLivestock.svelte';
+	import TitleBlock from '$lib/components/general/TitleBlock.svelte';
 
+	let activeTab = $state('marketReport');
+</script>
 
-<div role="alert" class="alert alert-success">
-  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-  </svg>
-  <span>Your purchase has been confirmed!</span>
+<div>
+		<TitleBlock>Available Animals</TitleBlock>
+
 </div>
