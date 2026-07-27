3b3b42a3b5ab07f542f10397f58102464deb1e43
MAtterholt <matterholt.dev@gmail.com>
2026-07-27 08:46:14 -0400

    tab links

--- livestockWeightTracker/src/routes/+layout.svelte
+++ livestockWeightTracker/src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+    import { page } from '$app/state';
+
     import "../style/app.css";
 	import favicon from '$lib/assets/favicon.svg';
 	let { children } = $props();
 
 	let activeTab = $state('available')
+
+
+console.log(page.url.pathname )
 </script>
 
 <svelte:head>
@@ -11,9 +16,10 @@
 </svelte:head>
 
     <div role="tablist" class="tabs tabs-border">
-      <a href="livestock" role="tab" class="tab">Tab 1</a>
-      <a href="market_report" role="tab" class="tab tab-active">Tab 2</a>
-      <a href="available"role="tab" class="tab">Tab 3</a>
-      <a href="sold"role="tab" class="tab">Tab 3</a>
+      <a href="livestock" role="tab"  class={["tab",page.url.pathname === '/livestock'?'tab-active' : "" ] }>livestock</a>
+      <a href="market_report" role="tab" class={["tab",page.url.pathname === '/market_report'?'tab-active' : "" ] }>market_report</a>
+    <a href="available" role="tab"  class={["tab",page.url.pathname === '/available'? 'tab-active' : "" ] }>available</a>
+    <a href="sold"role="tab"   class={["tab",page.url.pathname === '/sold'? 'tab-active' : "" ] }>sold</a>
     </div>
 {@render children()}
+``
