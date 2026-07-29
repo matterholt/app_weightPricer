33a718aa308dd556a1dbc06b8843ac9934ff6ec4
MAtterholt <matterholt.dev@gmail.com>
2026-07-29 07:51:51 -0400

    clean up remove unused

--- myappsvelte/src/lib/components/feature/animal_dialog/inputstate.svelte.js
+++
@@ -1,1 +0,0 @@
-export let editEntry = $state({ input: '' });
--- myappsvelte/src/lib/components/general/TitleBlock.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	let { children } = $props();
-</script>
-
-<div class="grid place-content-center m-4">
-	<h2 class="font-extrabold text-2xl w-fit">
-		{@render children?.()}
-	</h2>
-</div>
